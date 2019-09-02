4b5735cc95d0035708412edf72573ad8046e5ecf
Asad Jamil <asad.jamil07@gmail.com>
2019-09-02 22:01:17 +0500

    Initial Commit

---
+++ PopUpView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PopUpView
+//
+//  Created by Asad Jamil on 02/09/2019.
+//  Copyright © 2019 Asad Jamil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
